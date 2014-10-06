85d86e840d1335d9300b310b37f499333367d998
Emre Tanirgan <tanirganemre@gmail.com>
2014-10-06 12:13:03 -0400

    Added blocks and rows

--- ProjectFolders/Assets/NoteScript.cs
+++ ProjectFolders/Assets/NoteScript.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Block : MonoBehaviour{
+public class NoteScript : MonoBehaviour {
+
 	public float value;
 	public string pitch;
 	// Use this for initialization
-	public Block(float v, string p){
+	public NoteScript(float v, string p){
 		value = v;
 		pitch = p;
 	}
-
-	void Start () {
 	
+	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
